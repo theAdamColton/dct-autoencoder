fixed normalization

autoencoder.py
@@ -8,6 +8,7 @@ import torchvision.transforms.v2 as transforms
 from na_vit import NaViT
 
 from util import (
+    StreamingMeanStd2D,
     calculate_perplexity,
     dct2,
     idct2,
@@ -19,11 +20,11 @@ class DCTAutoencoderTransformer(nn.Module):
         self,
         vq_model,
         image_channels: int=3,
-        depth:int =4,
+        depth:int =6,
         feature_channels: int=768,
         patch_size: int=32,
         # only take 75 of the dct features and pass to the encoder
-        dct_compression_factor: float = 0.75,
+        dct_compression_factor: float = 0.80,
         max_n_patches: int = 512,
     ):
         """
@@ -59,38 +60,50 @@ class DCTAutoencoderTransformer(nn.Module):
             dropout=0.1,
             emb_dropout=0.1,
             token_dropout_prob=None,  # token dropout of 10% (keep 90% of tokens)
+            norm_in=False,
         )
 
         self.vq_model = vq_model
         self.vq_model.accept_image_fmap = False
         self.vq_model.channel_last = True
 
-        self.ema_alpha = 1e-4
         max_res = patch_size * max_n_patches
         self.max_res = max_res
-        self.dct_mean = nn.Parameter(torch.zeros(max_res, max_res), requires_grad=False)
-        self.dct_std = nn.Parameter(torch.ones(max_res, max_res), requires_grad=False)
+        self.dct_stats = StreamingMeanStd2D(self.max_res, self.max_res)
+
+        self.dct_std_eps = 1e-3
 
 
-    def dct_norm(self, x:torch.Tensor, eps=1e-7):
-        *_, h, w = x.shape
-        mean = reduce(x, '... h w -> h w', 'mean')
-        std = reduce(x, '... h w -> h w', torch.var)
-
+    def dct_norm(self, x:List[torch.Tensor]):
+        # records stats of x
         if self.training:
-            ema_update_2d(self.dct_mean, mean, self.ema_alpha)
+            for im in x:
+                self.dct_stats(im)
 
         # normalizes
-        mean = self.dct_mean[:h, :w]
-        std = self.dct_std[:h, :w]
+        mean = self.dct_stats.mean
+        std = self.dct_stats.std
+        out = []
 
-        return (x - mean) / torch.clamp(std, eps)
+        while len(x) > 0:
+            image = x.pop(0)
+            _,h,w = image.shape
+            out.append((image - mean[:h, :w]) / torch.clamp(std[:h, :w], self.dct_std_eps))
 
-    def dct_unnorm(self, x:torch.Tensor):
-        *_, h, w = x.shape
-        mean = self.dct_mean[:h, :w]
-        std = self.dct_std[:h, :w]
-        return (x+mean) * std
+        return out
+
+    def dct_unnorm(self, x:List[torch.Tensor]):
+        # xnorm = (x-mean) / std
+        # x = xnorm*std + mean
+        mean = self.dct_stats.mean
+        std = self.dct_stats.std
+        
+        out = []
+        while len(x) > 0:
+            im = x.pop(0)
+            _, h, w = im.shape
+            out.append((im * torch.clamp(std[:h, :w], self.dct_std_eps) + mean[:h, :w]))
+        return out
 
     @torch.no_grad()
     def prepare_batch(self, batch: List[torch.Tensor]):
@@ -108,13 +121,19 @@ class DCTAutoencoderTransformer(nn.Module):
             # rather than a rectangle
 
             c, h, w = x.shape
+
+            assert c == self.image_channels
+
             if h < self.patch_size:
                 rz = transforms.Resize((self.patch_size, int((self.patch_size / h) * w)))
                 x = rz(x)
-            elif w < self.patch_size:
+            if w < self.patch_size:
                 rz = transforms.Resize((int((self.patch_size / w) * h), self.patch_size))
                 x = rz(x)
             c, h, w = x.shape
+
+            assert h >= self.patch_size
+            assert w >= self.patch_size
 
             original_sizes.append((h,w))
 
@@ -156,8 +175,6 @@ class DCTAutoencoderTransformer(nn.Module):
             assert dct_h >= self.patch_size
             assert dct_w >= self.patch_size
 
-            assert h >= self.patch_size
-            assert w >= self.patch_size
 
             x_dct = x_dct[..., :dct_h, :dct_w]
 
@@ -178,14 +195,19 @@ class DCTAutoencoderTransformer(nn.Module):
         else:
             x = dct_features
 
-        x = [self.dct_norm(feat) for feat in x]
+        x = self.dct_norm(x)
 
+        assert not any([torch.isnan(im).any().item() for im in x])
 
         encoder_out = self.encoder(x,
              group_images = True,
-             group_max_seq_len = self.max_n_patches*2,
+             group_max_seq_len = 2048,
         )
+
         z = encoder_out['x']
+
+        assert not torch.isnan(z).any().item()
+
         mask = encoder_out['key_pad_mask']
         revert_patching = encoder_out['revert_patching']
         patches = encoder_out['patches']
@@ -237,6 +259,9 @@ class DCTAutoencoderTransformer(nn.Module):
         x_images = []
 
         with torch.no_grad():
+            # un normalize all tensors in x and z
+            x = self.dct_unnorm(x)
+            z = self.dct_unnorm(z)
             for x_dct_image, z_dct_image, (h,w) in zip(x, z, original_sizes):
 
                 #z_dct_image = rearrange(z_dct_image, '(h w) (c p1 p2) -> c (h p1) (w p2)', p1 = self.patch_size, p2=self.patch_size, w=w)
@@ -249,11 +274,6 @@ class DCTAutoencoderTransformer(nn.Module):
                     padded[:, :ih, :iw] = to_pad
                     return padded
         
-
-                # un normalize
-                x_dct_image = self.dct_unnorm(x_dct_image)
-                z_dct_image = self.dct_unnorm(z_dct_image)
-
 
                 # pads back to image size
                 x_dct_image = pad(x_dct_image, h,w)

main.py
@@ -52,6 +52,23 @@ def make_image_grid(x, x_hat, filename=None, n: int = 10, max_size:int=1024):
 
     return im
 
+def get_collate(vq_autoencoder):
+    def dict_collate(x: List[dict]):
+        o = {}
+        for d in x:
+            for k,v in d.items():
+                l = o.get(k)
+                if l is not None:
+                    o[k].append(v)
+                else:
+                    o[k] = [v]
+        dct_features, original_sizes = vq_autoencoder.prepare_batch(o['pixel_values'])
+
+        o['dct_features'] = dct_features
+        o['original_sizes'] = original_sizes
+
+        return o
+    return dict_collate
 
 def validate(
     vq_autoencoder,
@@ -62,7 +79,7 @@ def validate(
     use_wandb: bool = False,
 ):
     vq_autoencoder.eval()
-    dataloader = DataLoader(val_ds, batch_size=batch_size, num_workers=8, collate_fn=None)
+    dataloader = DataLoader(val_ds, batch_size=batch_size, num_workers=0, collate_fn=get_collate(vq_autoencoder))
     for i, batch in enumerate(tqdm(dataloader)):
         x = batch["pixel_values"]
         x = x.to(device)
@@ -97,7 +114,7 @@ def train(
     epochs: int = 1,
     device="cuda",
     dtype=torch.float16,
-    log_every=10,
+    val_every=10,
     n_log:int=10,
     max_steps=1e20,
     warmup_steps=10,
@@ -107,26 +124,12 @@ def train(
 
     n_steps = 0
 
-    def dict_collate(x: List[dict]):
-        o = {}
-        for d in x:
-            for k,v in d.items():
-                l = o.get(k)
-                if l is not None:
-                    o[k].append(v)
-                else:
-                    o[k] = [v]
-        dct_features, original_sizes = vq_autoencoder.prepare_batch(o['pixel_values'])
-
-        o['dct_features'] = dct_features
-        o['original_sizes'] = original_sizes
-
-        return o
+    collate = get_collate(vq_autoencoder)
 
     for epoch_i, epoch in enumerate(range(epochs)):
         train_ds = train_ds.shuffle(seed=epoch_i + 42)
         dataloader = DataLoader(
-                train_ds, batch_size=batch_size, num_workers=0, collate_fn=dict_collate
+                train_ds, batch_size=batch_size, num_workers=0, collate_fn=collate
         )
         for i, batch in enumerate(tqdm(dataloader)):
             if epoch_i == 0 and i < warmup_steps:
@@ -167,12 +170,16 @@ def train(
                     }
                 )
 
-            if i % log_every == 0:
+            if i % val_every == 0:
+                print("logging images ....")
+                vq_autoencoder.eval()
                 with torch.no_grad():
-                    print("logging....")
-                    vq_autoencoder.eval()
-                    out = vq_autoencoder(dct_features=dct_features[:n_log], original_sizes=original_sizes[:n_log], decode=True)
-                    vq_autoencoder.train()
+                    batch = collate([train_ds[i] for i in range(n_log)])
+                    dct_features, original_sizes = batch['dct_features'], batch['original_sizes']
+                    dct_features = [x.to(device).to(dtype) for x in dct_features]
+                    with torch.autocast(device):
+                        out = vq_autoencoder(dct_features=dct_features, original_sizes=original_sizes)
+                vq_autoencoder.train()
                 image = make_image_grid(out['x'], out['x_hat'], filename=f"{OUTDIR}/train image {i:04}.png")
                 if use_wandb:
                     wandb.log(
@@ -215,11 +222,11 @@ def main(
     image_dataset_path_or_url="imagenet-1k",
     device="cuda",
     batch_size: int = 32,
+    patch_size: int = 32,
+    feature_channels:int = 768,
+    dct_compression_factor:float=0.75,
     use_wandb: bool = False,
 ):
-    feature_channels = 768
-    patch_size = 16
-    dct_compression_factor=0.75
 
     ds_train = load_and_transform_dataset(
         image_dataset_path_or_url, split="train",
@@ -251,13 +258,15 @@ def main(
             dct_compression_factor=dct_compression_factor,
         ).to(device)
 
-    codebook_sizes = [256]
+    codebook_size = 256
 
-    head_numbers = [16]
+    heads = 16
 
 
-    for codebook_size in codebook_sizes:
-        for heads in head_numbers:
+#    for codebook_size in codebook_sizes:
+#        for heads in head_numbers:
+    for learning_rate in [5e-4, 1e-3, 3e-3]:
+        for patch_size in [32, 16]:
             vq_autoencoder = get_vq_autoencoder(codebook_size, heads)
 
             run_d = dict(
@@ -266,6 +275,7 @@ def main(
                 bits=heads * math.log2(codebook_size),
                 feature_dim=feature_channels,
                 patch_size=patch_size,
+                learning_rate=learning_rate,
             )
 
             print("starting run: ", run_d)

na_vit.py
@@ -188,7 +188,7 @@ class Transformer(nn.Module):
         return self.norm(x)
 
 class NaViT(nn.Module):
-    def __init__(self, *, image_size, patch_size, dim, depth, heads, mlp_dim, channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0., token_dropout_prob = None, ):
+    def __init__(self, *, image_size, patch_size, dim, depth, heads, mlp_dim, channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0., token_dropout_prob = None, norm_in:bool = True):
         super().__init__()
         image_height, image_width = pair(image_size)
 
@@ -217,7 +217,7 @@ class NaViT(nn.Module):
         self.patch_size = patch_size
 
         self.to_patch_embedding = nn.Sequential(
-            LayerNorm(patch_dim),
+            LayerNorm(patch_dim) if norm_in else nn.Identity(),
             nn.Linear(patch_dim, dim),
             LayerNorm(dim),
         )
@@ -228,20 +228,6 @@ class NaViT(nn.Module):
         self.dropout = nn.Dropout(emb_dropout)
 
         self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
-
-        # final attention pooling queries
-
-        #self.attn_pool_queries = nn.Parameter(torch.randn(dim))
-        #self.attn_pool = Attention(dim = dim, dim_head = dim_head, heads = heads)
-
-        ## output to logits
-
-        #self.to_latent = nn.Identity()
-
-        #self.mlp_head = nn.Sequential(
-        #    LayerNorm(dim),
-        #    nn.Linear(dim, num_classes, bias = False)
-        #)
 
     @property
     def device(self):
@@ -329,7 +315,9 @@ class NaViT(nn.Module):
 
         # combine patched images as well as the patched width / height positions for 2d positional embedding
 
+
         patches = pad_sequence(batched_sequences)
+
         patch_positions = pad_sequence(batched_positions)
 
         # need to know how many images for final attention pooling
@@ -391,39 +379,3 @@ class NaViT(nn.Module):
                     
 
         return dict(x=x, revert_patching=revert_patching, key_pad_mask=key_pad_mask, patches=patches)
-
-
-
-#        # do attention pooling at the end
-#
-#
-#        queries = repeat(self.attn_pool_queries, 'd -> b n d', n = max_queries, b = x.shape[0])
-#
-#        # attention pool mask
-#
-#
-#        attn_pool_mask = rearrange(image_id_arange, 'i -> i 1') == rearrange(batched_image_ids, 'b j -> b 1 j')
-#
-#        attn_pool_mask = attn_pool_mask & rearrange(key_pad_mask, 'b j -> b 1 j')
-#
-#        attn_pool_mask = rearrange(attn_pool_mask, 'b i j -> b 1 i j')
-#
-#        # attention pool
-#
-#        x = self.attn_pool(queries, context = x, attn_mask = attn_pool_mask) + queries
-#
-#        x = rearrange(x, 'b n d -> (b n) d')
-#
-#
-#        # each batch element may not have same amount of images
-#
-#        is_images = image_id_arange < rearrange(num_images, 'b -> b 1')
-#        is_images = rearrange(is_images, 'b n -> (b n)')
-#
-#        x = x[is_images]
-#
-#        # project out to logits
-#
-#        x = self.to_latent(x)
-#
-#        return self.mlp_head(x)