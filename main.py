fixed normalization; converges quickly

main.py
@@ -58,40 +58,6 @@ def make_image_grid(x, x_hat, filename=None, n: int = 10, max_size:int=1024):
     return im
 
 
-def validate(
-    vq_autoencoder,
-    val_ds,
-    batch_size: int = 32,
-    device="cuda",
-    dtype=torch.float16,
-    use_wandb: bool = False,
-):
-    vq_autoencoder.eval()
-    dataloader = DataLoader(val_ds, batch_size=batch_size, num_workers=0, collate_fn=dict_collate)
-    for i, batch in enumerate(tqdm(dataloader)):
-        x = batch["pixel_values"]
-        x = x.to(device)
-        with torch.no_grad():
-            with torch.autocast(device):
-                out = vq_autoencoder(x, decode=True)
-        if use_wandb:
-            wandb.log(
-                {
-                    "val": dict(
-                        step=i,
-                        rec_loss=out["rec_loss"].item(),
-                        commit_loss=out["commit_loss"].item(),
-                        perplexity=out["perplexity"].item(),
-                        pixel_loss=out["pixel_loss"].item(),
-                    )
-                }
-            )
-
-        if i == 0:
-            image = make_image_grid(out['x'], out['x_hat'], filename=f"{OUTDIR}/val image {i:05}.png")
-            if use_wandb:
-                wandb.log({"val": dict(image=wandb.Image(image))})
-
 
 def train(
     vq_autoencoder: DCTAutoencoderTransformer,
@@ -126,7 +92,7 @@ def train(
 
             dct_features = batch["dct_features"]
             original_sizes = batch["original_sizes"]
-            dct_features = [x.to(device) for x in dct_features]
+            dct_features = [x.to(dtype).to(device) for x in dct_features]
 
             with torch.autocast(device, dtype=dtype):
                 out = vq_autoencoder(dct_features=dct_features, original_sizes=original_sizes, decode=False)
@@ -282,14 +248,6 @@ def main(
                 batch_size=batch_size,
                 use_wandb=use_wandb,
             )
-            #validate(
-            #    vq_autoencoder,
-            #    ds_test,
-            #    device=device,
-            #    dtype=dtype,
-            #    batch_size=batch_size,
-            #    use_wandb=use_wandb,
-            #)
 
             vq_autoencoder = None
 

na_vit.py
@@ -15,21 +15,54 @@ from einops.layers.torch import Rearrange
 
 # helpers
 
+def scatter_add_2d(x: torch.Tensor, pos_h: torch.LongTensor, pos_w: torch.LongTensor, y: Optional[torch.Tensor]=None):
+    """
+    x: 3d tensor
+
+    adds y to x in the indices indicated by y along
+    the last two dimensions
+
+    in place
+    """
+    h, w, z = x.shape
+    pos_flat = (pos_h * w + pos_w).flatten()
+    pos_flat = repeat(pos_flat, 'n -> n z', z=z)
+    if y is None:
+        y = torch.ones_like(pos_flat, dtype=x.dtype)
+    x.view(h*w, z).scatter_add_(0, pos_flat, y)
+
 class PatchNorm(nn.Module):
+    """
+    Records statistics of patch pixels,
+    uses https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_online_algorithm
+    to record estimates of mean and std
+    """
     def __init__(self, patch_height_dim: int, patch_width_dim: int, patch_dim:int, eps:float=1e-1):
         super().__init__()
         self.eps=eps
-        self.weights = nn.Parameter(torch.ones(patch_height_dim, patch_width_dim, patch_dim))
-        self.biases = nn.Parameter(torch.zeros(patch_height_dim, patch_width_dim, patch_dim))
         self.patch_dim = patch_dim
+
+        self.n = nn.Parameter(torch.zeros(patch_height_dim,patch_width_dim,), requires_grad=False)
+        self.mean = nn.Parameter(torch.zeros(patch_height_dim, patch_width_dim, patch_dim),requires_grad=False)
+        self.m2 = nn.Parameter(torch.zeros(patch_height_dim, patch_width_dim, patch_dim), requires_grad=False)
+
+    @property
+    def var(self):
+        mask = self.n == 0
+        var = self.m2.clamp(self.eps, 1e6) / self.n.unsqueeze(-1).clamp(1.0)
+        var[mask] = 1.0
+        return var
+
+    @property
+    def std(self):
+        return self.var.sqrt()
+
 
     def forward(self, patches: torch.Tensor, pos_h: torch.LongTensor, pos_w: torch.LongTensor, key_pad_mask: torch.BoolTensor):
         """
         normalizes patches using patch wieghts and biases that are indexed by pos_h and pos_w
 
         patches should be (..., dim)
-
-        TODO is masking necessary?
         """
         patches_shape = patches.shape
          
@@ -40,12 +73,28 @@ class PatchNorm(nn.Module):
         pos_w = pos_w[~key_pad_mask]
         patches = patches[~key_pad_mask]
 
-        weights = self.weights[pos_h, pos_w, :]
-        biases = self.biases[pos_h, pos_w, :]
-        patches = F.layer_norm(patches, weights.shape, weights, biases, self.eps)
+        if self.training:
+            with torch.no_grad():
+                # updates n by incrementing all values at pos_h and pos_w
+                scatter_add_2d(self.n.unsqueeze(-1), pos_h, pos_w)
+
+
+                # updates the mean
+                delta = patches - self.mean[pos_h, pos_w]
+                scatter_add_2d(self.mean, pos_h, pos_w, delta / self.n[pos_h, pos_w, None])
+
+                delta2 = patches - self.mean[pos_h, pos_w]
+                scatter_add_2d(self.m2, pos_h, pos_w, delta * delta2)
+
+
+        patches = (patches - self.mean[pos_h, pos_w]) / self.std[pos_h, pos_w]
+
         out = torch.zeros(patches_shape, dtype=patches.dtype, device=patches.device)
         out[~key_pad_mask] = patches
         return out
+
+    def inverse_norm(self, patches: torch.Tensor, pos_h: torch.LongTensor, pos_w: torch.LongTensor):
+        return patches * self.std[pos_h, pos_w] + self.mean[pos_h, pos_w]
 
 
 def exists(val):
@@ -367,16 +416,16 @@ class NaViT(nn.Module):
         h_indices, w_indices = patch_positions.unbind(dim = -1)
 
         # normalizes patches
-        x = self.patch_norm(patches, h_indices, w_indices, key_pad_mask)
+        patches = self.patch_norm(patches, h_indices, w_indices, key_pad_mask)
 
         # possibly adds pos embedding info before projecting in
         if self.pos_embed_before_proj:
             h_pos = self.pos_embed_height[h_indices]
             w_pos = self.pos_embed_width[w_indices]
-            x = x + h_pos + w_pos
+            x = patches + h_pos + w_pos
             x = self.to_patch_embedding(x)        
         else:
-            x = self.to_patch_embedding(x)        
+            x = self.to_patch_embedding(patches)        
             h_pos = self.pos_embed_height[h_indices]
             w_pos = self.pos_embed_width[w_indices]
             x = x + h_pos + w_pos